53fc3d08be5e45eb2625f74d223eb3c17d09e0f2
suresh-star <75731863+suresh-star@users.noreply.github.com>
2024-04-15 22:18:15 +0530

    Update SampleApplication.java

--- src/main/java/com/example/sample/SampleApplication.java
+++ src/main/java/com/example/sample/SampleApplication.java
@@ -1,12 +1,11 @@
 package com.example.sample;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 
 
-@SpringBootApplication
 @RestController
 public class SampleApplication {
 
