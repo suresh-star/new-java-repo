42cb54d3c5bed760ca602f504f03a40382f774fa
suresh-star <75731863+suresh-star@users.noreply.github.com>
2024-04-15 22:17:41 +0530

    Update ServletInitializer.java

--- src/main/java/com/example/sample/ServletInitializer.java
+++ src/main/java/com/example/sample/ServletInitializer.java
@@ -2,7 +2,7 @@ package com.example.sample;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
+@SpringBootApplication
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
